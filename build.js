233c9c12acda6a02f114ab55ed7608a95468bed8
Divyansh1508 <divyanshjain1508@gmail.com>
2026-09-22 23:20:50 +0530

    Add build.js to generate dist folder for Netlify deployment

---
+++ build.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = __dirname;
+const distDir = path.join(rootDir, 'dist');
+
+// Clean and recreate dist
+if (fs.existsSync(distDir)) {
+  fs.rmSync(distDir, { recursive: true, force: true });
+}
+fs.mkdirSync(distDir, { recursive: true });
+
+// Copy all static assets and pages to dist
+const itemsToCopy = [
+  'index.html',
+  'about.html',
+  'blog.html',
+  'career.html',
+  'contact.html',
+  'projects.html',
+  'services.html',
+  'robots.txt',
+  'sitemap.xml',
+  'favicon.ico',
+  'css',
+  'js',
+  'images'
+];
+
+for (const item of itemsToCopy) {
+  const src = path.join(rootDir, item);
+  const dest = path.join(distDir, item);
+  if (fs.existsSync(src)) {
+    fs.cpSync(src, dest, { recursive: true });
+    console.log(`Copied ${item} -> dist/${item}`);
+  }
+}
+
+console.log('Fitscalez build successful: dist/ is ready.');
